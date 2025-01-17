802261c1db66578ee8f49b5b5357ad4ea1f3ca38
Igaro-Franklin <igarofranklin@gmail.com>
2025-01-17 10:13:59 -0300

    Enviou repositório inicial do projeto em ruby on rails

---
+++ app/controllers/produtos_controller.rb
@@ -0,0 +1,51 @@
+class ProdutosController < ApplicationController
+  before_action :set_produto, only: %i[show edit update destroy]
+  
+
+  def index
+    @produtos = Produto.all
+  end
+
+  def show
+  end
+
+  def new
+    @produto = Produto.new
+  end
+
+  def create
+    @produto = Produto.new(produto_params)
+    if @produto.save
+      redirect_to @produto
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @produto.update(produto_params)
+      redirect_to @produto
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @produto.destroy
+    redirect_to produtos_path
+  end
+
+  private
+
+    def produto_params
+      params.expect(produto: [ :nome ])
+    end
+
+    def set_produto
+      @produto = Produto.find(params[:id])
+    end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :produtos
+
+  root 'produtos#index'
+end
